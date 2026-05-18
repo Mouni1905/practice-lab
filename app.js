7f463f374d3f57f5165ab635713b9a5f067ab8bd
Mouni1905 <103523832+Mouni1905@users.noreply.github.com>
2026-05-18 11:37:06 +0530

    Update response message to indicate success

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("CI/CD Lab Working 🚀");
+  res.end("CI/CD Lab Working Successfully 🚀");
 });
 
 server.listen(process.env.PORT || 3000);
