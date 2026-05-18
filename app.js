dda30ed06b09393ad0623e9338d59661998e06b7
Mouni <mouni5867@gmail.com>
2026-05-18 16:17:55 +0530

    Added Node application

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("CI/CD Lab Working Successfully 🚀");
+  res.end("CI/CD Terminal Lab 🚀");
 });
 
 server.listen(process.env.PORT || 3000);
